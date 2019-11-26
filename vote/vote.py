fd026f311bc6197c437b269f2bde35342c2bf77b
LUKEVAUGHAN-PC\Luke Vaughan <luke@swiftrefurbishments.co.uk>
2019-11-26 18:28:48 +0000

    Implemented removing on low votes and pinning on high votes

--- vote/vote.py
+++ vote/vote.py
@@ -3,6 +3,11 @@ from redbot.core import commands
 from redbot.core import Config
 import discord
 
+listener = getattr(commands.Cog, "listener", None)  # Trusty + Sinbad
+if listener is None:
+
+    def listener(name=None):
+        return lambda x: x
 
 class vote(commands.Cog):
     """Adds a voting system to a channel"""
@@ -25,7 +30,7 @@ class vote(commands.Cog):
         self.config.register_channel(**default_channel)
 
     @commands.command()
-    async def addchannel(self, ctx, upemote: discord.Emoji, downemote: discord.Emoji, remove: int, pin: int):
+    async def addchannel(self, ctx, upemote: discord.Emoji, downemote: discord.Emoji, pin: int, remove: int):
         """Activates the voting system in the current channel"""
         channels = await self.config.guild(ctx.guild).voting()
         try:
@@ -40,10 +45,38 @@ class vote(commands.Cog):
 
     @commands.command()
     async def removechannel(self, ctx):
-        """Activates the voting system in the current channel"""
+        """Removes the voting system in the current channel"""
         channels = await self.config.guild(ctx.guild).voting()
         channels.remove(ctx.channel.id)
         await self.config.guild(ctx.guild).voting.set(channels)
         await self.config.channel(ctx.channel).clear()
+
+    @listener()
+    async def on_message(self, message):
+        channels = await self.config.guild(message.guild).voting()
+        if message.channel.id in channels:
+            await message.add_reaction(self.bot.get_emoji(await self.config.channel(message.channel).upemote()))
+            await message.add_reaction(self.bot.get_emoji(await self.config.channel(message.channel).downemote()))
+
+    @listener()
+    async def on_reaction_add(self, reaction, user):
+        channels = await self.config.guild(reaction.message.guild).voting()
+        if reaction.message.channel.id in channels:
+            up = await self.config.channel(reaction.message.channel).upemote()
+            down = await self.config.channel(reaction.message.channel).downemote()
+            pin = await self.config.channel(reaction.message.channel).downemote()
+            remove = await self.config.channel(reaction.message.channel).downemote()
+            if reaction.emoji.id == up or reaction.emoji.id == down:
+                if user == reaction.message.author:
+                    await reaction.remove(user)
+                if reaction.emoji.id == up and pin != 0:
+                    if reaction.count < pin:
+                        print("pinned a great meme")
+                        await reaction.message.pin()
+                if reaction.emoji.id == down and remove != 0:
+                    if reaction.count < remove:
+                        print("deleted a bad meme")
+                        await reaction.message.delete()
+
 
 
