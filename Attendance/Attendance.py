457ebdb5a96e4134c8e81d32cc0961f9f4517d71
LUKEVAUGHAN-PC\Luke Vaughan <luke@swiftrefurbishments.co.uk>
2020-03-30 19:54:27 +0100

    Variety of fixes and changes

--- Attendance/Attendance.py
+++ Attendance/Attendance.py
@@ -1,64 +1,62 @@
 from __future__ import print_function
 from redbot.core import Config
 from redbot.core import commands
-import datetime
-import requests
-import random
-import discord
-import typing
-import asyncio
-import time
-import copy
 import discord
 import gspread
+import time
 from oauth2client.service_account import ServiceAccountCredentials
-#from discord.ext import commands
 
 
 class Attendance(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        Creds = ServiceAccountCredentials.from_json_keyfile_name("GCreds.json", scope)
-        gc = gspread.authorize(Creds)
+
 
     @commands.command()
-    async def LogEvent(ctx, mem1: discord.Member = "", mem2: discord.Member = "", mem3: discord.Member = "",
-                mem4: discord.Member = "", mem5: discord.Member = ""):
+    async def LogEvent(self, ctx, *guests: discord.Member):
+        """Incremements members in the event channel's event score, minus guests"""
         vch = ctx.guild.get_channel(360785714339643392)
-        TempArr = [mem1, mem2, mem3, mem4, mem5]
         Guest_Arr = []
-        Member_Array = []
-        for temp in TempArr:
-            if temp != "":
-                Guest_Arr.append(f"{temp.name}#{temp.discriminator}")
-        mem = vch.members
-        for x in mem:
-            y = f"{x.name}#{x.discriminator}"
-            Member_Array.append(y)
-        print(Member_Array)
-        print(Guest_Arr)
-        for Guest in Guest_Arr:
-            if Guest in Member_Array:
-                Member_Array.remove(Guest)
+        error = False
+        for member in guests:
+            if member:
+                Guest_Arr.append(str(member))
+            else:
+                await ctx.send(str(member) + " is not a member")
+                error = True
+                break
 
-        print(Member_Array)
-        if len(Member_Array) == 0:
-            print("Only Guests/Empty Channel")
-        else:
-            wks = gc.open("GoA Guild Roster").sheet1
-            for member in Member_Array:
-                cell = wks.find(member)
-                col = cell.col
-                row = cell.row
-                AC = wks.cell(row, 10)
-                #print(wks.cell(row,4))
-                ACV = int (AC.value)
-                #print(ACV)
-                ACV = ACV+1
-                #print(ACV)
-                wks.update_cell(AC.row, AC.col, ACV)
-                today = date.today()
-                d1 = today.strftime("%d/%m/%Y")
-                #print(wks.cell(row, 11))
-                wks.update_cell(AC.row, 11, d1)
+        if not error:
+            Member_Array = []
+            mem = vch.members
+            for x in mem:
+                Member_Array.append(str(x))
+            print(Member_Array)
+            print(Guest_Arr)
+            for Guest in Guest_Arr:
+                if Guest in Member_Array:
+                    Member_Array.remove(Guest)
+
+            print(Member_Array)
+            if len(Member_Array) == 0:
+                await ctx.send("Only Guests/Empty Channel")
+            else:
+                scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+                Creds = ServiceAccountCredentials.from_json_keyfile_name("GCreds.json", scope)
+                gc = gspread.authorize(Creds)
+                wks = gc.open("GoA Guild Roster").sheet1
+                for member in Member_Array:
+                    await ctx.send(member)
+                    cell = wks.find(member)
+                    col = cell.col
+                    row = cell.row
+                    AC = wks.cell(row, 10)
+                    #print(wks.cell(row,4))
+                    ACV = int (AC.value)
+                    #print(ACV)
+                    ACV = ACV+1
+                    #print(ACV)
+                    wks.update_cell(AC.row, AC.col, ACV)
+                    d1 = time.strftime("%d/%m/%Y")
+                    #print(wks.cell(row, 11))
+                    wks.update_cell(AC.row, 11, d1)
